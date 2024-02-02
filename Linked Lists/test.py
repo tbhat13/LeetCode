5f348472a841742f66f08b7979b2afaf567503ed
TBhatPython <bhat.tushar@outlook.com>
2024-02-01 17:29:28 -0800

    r

--- Linked Lists/test.py
+++ Linked Lists/test.py
@@ -1,11 +1,11 @@
-from removeLinkedListElems import Solution, ListNode
+from reverseLinkedList import Solution, ListNode
 
 def main():
     list1 = ListNode(1, ListNode(1, ListNode(4)))
     #list2 = ListNode(2, ListNode(3, ListNode(4)))
-    val = 4
+    #val = 4
     tester = Solution()
-    print(tester.removeElements(list1, val))
+    print(tester.removeElements(list1))
 
 if __name__ == "__main__":
     main()
