57d4b8af64c06ef90dc6b476399216bf95b9cf84
TBhatPython <bhat.tushar@outlook.com>
2024-02-01 16:32:34 -0800

    linkedListIntersection.py testing

---
+++ Linked Lists/linkedListIntersection.py
@@ -0,0 +1,22 @@
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        iter = set()
+        current = headA
+
+        while current:
+            iter.add(current)
+            current = current.next
+
+        current = headB
+
+        while current:
+            if current in iter:
+                return current.val
+            else:
+                iter.add(current)
+                current = current.next
--- Linked Lists/test.py
+++ Linked Lists/test.py
@@ -1,10 +1,10 @@
-from removeDuplicates import Solution, LinkNode
+from linkedListIntersection import Solution, ListNode
 
 def main():
-    list1 = LinkNode(1, LinkNode(1, LinkNode(4)))
-    #list2 = ListNode(2, ListNode(3, ListNode(4)))
+    list1 = ListNode(1, ListNode(1, ListNode(4)))
+    list2 = ListNode(2, ListNode(3, ListNode(4)))
     tester = Solution()
-    print(tester.deleteDuplicates(list1))
+    print(tester.getIntersectionNode(list1, list2))
 
 if __name__ == "__main__":
     main()
