4148f7eb0bfe1f26bd387b430c4010399daf681b
TBhatPython <bhat.tushar@outlook.com>
2024-02-03 13:07:03 -0800

    addTwoNumber.py

--- Linked Lists/addTwoNumbers.py
+++ Linked Lists/addTwoNumbers.py
@@ -1,16 +1,17 @@
+from llist import sllist
+
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-    def __str__(self, ll):
-        result = []
+    def ll_to_list(self, ll):
+        list = []
         current = ll
         while current:
-            result.append(current.val)  
+            list.append(current.val)
             current = current.next
-
-        return result
+        return list
 
 
 class Solution(object):
@@ -37,4 +38,20 @@ class Solution(object):
                 l2 = l2.next
 
         return ListNode().__str__(head)
+    
+class OtherSolution(object):
+    def addTwoNumbers(self, l1, l2):
+        list1 = sllist(ListNode().ll_to_list(l1))
+        list2 = sllist(ListNode().ll_to_list(l2))
+        result = []
+        carry = 0
+        for value1, value2 in zip(list1, list2):
+            added = value1 + value2 + carry
+            if added >= 10:
+                carry = 1
+            else:
+                carry = 0
+            result.append(added % 10)
+
+        return sllist(result)
     
