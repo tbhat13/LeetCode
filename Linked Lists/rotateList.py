6ea3c16e0444c82ee6478b3e71636b711ea0c55d
TBhatPython <bhat.tushar@outlook.com>
2024-02-05 16:20:43 -0800

    rotateList.py

--- Linked Lists/rotateList.py
+++ Linked Lists/rotateList.py
@@ -1,5 +1,4 @@
 from llist import sllist
-
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
@@ -16,24 +15,25 @@ class ListNode(object):
 class Solution(object):
     def rotateRight(self, head, k):
         if k == 0 or head is None or head.next is None: return head
-        current = head
+        current = length_finder = head
         length = 0
-        while current.next.next:
-            current = current.next
+        while length_finder:
+            length_finder = length_finder.next
             length+= 1
-        length+=2
         if k % length == 0: return head
-        for _ in range(k % length):
+        for _ in range(k%length):
+            while current.next.next:
+                current = current.next
             new_head = current.next
             current.next = None
             new_head.next = head
             current = head = new_head
-            
+        
         return new_head
     
-
 class OtherSolution(object):
     def rotateRight(self, head, k):
         list1 = sllist(ListNode().display(head))
         list1.rotate(k)
         return list1
+    
