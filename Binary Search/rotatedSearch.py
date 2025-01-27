934110844f374a7747efb772dd80e09f89b1f9f4
tbhat13 <bhat.tushar@outlook.com>
2025-01-27 12:36:27 -0500

    rotatedSearch start

---
+++ Binary Search/rotatedSearch.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def search(self, nums, target):
+        nums.sort()
+        print(nums)
+
+
+
+
+
+def main():
+    sol = Solution()
+    nums = [4,5,6,7,0,1,2]
+    sol.search(nums, 0)
